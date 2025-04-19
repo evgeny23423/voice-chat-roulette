d37fc61caa6b90d53d94ed83d4fd651a6f53d20e
sogetlifted@gmail.com <sogetlifted@gmail.com>
2025-04-19 16:26:51 +0300

    обновленине

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { PeerServer } = require('peer');
 const cors = require('cors');
 const path = require('path');
-const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 9000;
